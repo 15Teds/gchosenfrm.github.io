0a21a0b33b6763354648f5eb13a94cb6ab6a798b
gchosenfrm <46633900+gchosenfrm@users.noreply.github.com>
2019-01-12 16:00:32 -0500

    Update info.js

--- info.js
+++ info.js
@@ -1,13 +1,15 @@
 console.log('Fetching Troop Info');
 var WebmasterInfo = [['Chair Name','Karen Bograd'],
+                     ['Chair Email', 'kbograd@troop1hopkinton.org'],
+                     ['Chair Phone','508-904-5139'],
                      ['Scoutmaster Name','Damian Packer'],
-                     ['Scoutmaster Email','<a href="mailto:scoutmaster@troop1hopkinton.org">'],
+                     ['Scoutmaster Email','<a href="mailto:scoutmaster@troop1hopkinton.org">scoutmaster@troop1hopkinton.org</a>'],
                      ['Scoutmaster Phone','(617) 909-9959'],
                      ['SPL Name','Aditya Kaushik'],
                      ['Webmaster Name','Daniel C-H']];
 localStorage.setItem('DevInfo',WebmasterInfo);
 
-var troopTags = document.getElementsByTagName('troop');
+var troopTags = document.getElementsByTagName('samp');
 for(var i = 0;i < troopTags.length;i++){
   var found = false;
   for(var i2 = 0;i2<WebmasterInfo.length;i2++){
