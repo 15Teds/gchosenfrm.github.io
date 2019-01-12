f652abf20eb3b93ad2ec4584ca6fa7c8cc5a7213
gchosenfrm <46633900+gchosenfrm@users.noreply.github.com>
2019-01-12 15:29:36 -0500

    Create info.js

---
+++ info.js
@@ -0,0 +1,6 @@
+console.log('Fetching Troop Info');
+localStorage.setItem('ScoutmasterName','Damian Packer');
+localStorage.setItem('ScoutmasterEmail','Damian Packer');
+localStorage.setItem('ScoutmasterPhone','Damian Packer');
+localStorage.setItem('SPLName','');
+localStorage.setItem('WebmasterName','');
