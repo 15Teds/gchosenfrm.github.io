2d8ec32b24378d21ed948f4c544e49e897e7513a
gchosenfrm <46633900+gchosenfrm@users.noreply.github.com>
2019-01-12 15:48:30 -0500

    Update info.js

--- info.js
+++ info.js
@@ -1,6 +1,20 @@
 console.log('Fetching Troop Info');
-localStorage.setItem('ScoutmasterName','Damian Packer');
-localStorage.setItem('ScoutmasterEmail','Damian Packer');
-localStorage.setItem('ScoutmasterPhone','Damian Packer');
-localStorage.setItem('SPLName','');
-localStorage.setItem('WebmasterName','');
+var WebmasterInfo = [['Chair Name','Karen Bograd'],
+                     ['Scoutmaster Name','Damian Packer'],
+                     ['Scoutmaster Email','<a href="mailto:scoutmaster@troop1hopkinton.org">'],
+                     ['Scoutmaster Phone','(617) 909-9959'],
+                     ['SPL Name','Aditya Kaushik'],
+                     ['Webmaster Name','Daniel C-H']];
+localStorage.setItem('DevInfo',WebmasterInfo);
+
+var troopTags = document.getElementsByTagName('troop');
+for(var i = 0;i < troopTags.length;i++){
+  var found = false;
+  for(var i2 = 0;i2<WebmasterInfo.length;i2++){
+    if(troopTags[i].className == WebmasterInfo[i2][0]){
+      troopTags[i].innerHTML = WebmasterInfo[i2][1];
+      found = true;
+    }
+  }
+  if(!found){console.warn('unidentified Troop Tag [Index: ' + i + ', classname: ' + troopTags[i].className + ']')}
+}
